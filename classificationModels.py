7b20f16601c9340dd987123285cca4db34cbcca4
William Zou <wyzou@uwaterloo.ca>
2020-12-11 20:11:20 -0500

    Modularize code

---
+++ classificationModels.py
@@ -0,0 +1,132 @@
+from pyspark.ml.linalg import Vector
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import HashingTF, StopWordsRemover, IDF, Tokenizer
+
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit, CrossValidator
+
+from pyspark.ml.classification import LinearSVC, LogisticRegression, NaiveBayes, GBTClassifier, DecisionTreeClassifier
+
+
+def trainLinearSVCModel(data): 
+  tokenizer = Tokenizer().setInputCol("comment_text").setOutputCol("words")
+  remover= StopWordsRemover().setInputCol("words").setOutputCol("filtered").setCaseSensitive(False)
+  hashingTF = HashingTF().setNumFeatures(1000).setInputCol("filtered").setOutputCol("rawFeatures")
+  idf = IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+  lr = LinearSVC(labelCol="label", featuresCol="features", maxIter=20)
+  pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, lr])
+
+  paramGrid = ParamGridBuilder()\
+      .addGrid(hashingTF.numFeatures,[1000]) \
+      .addGrid(lr.regParam, [0.1]) \
+      .build()
+
+  crossval = TrainValidationSplit(estimator=pipeline,
+                              estimatorParamMaps=paramGrid,
+                              evaluator=BinaryClassificationEvaluator().setMetricName('areaUnderPR'), # set area Under precision-recall curve as the evaluation metric
+                              # 80% of the data will be used for training, 20% for validation.
+                              trainRatio=0.8)
+
+  cvModel = crossval.fit(data)
+  modelName = "LinearSVCModel"
+  cvModel.bestModel.write().overwrite().save(modelName)
+
+  return modelName
+
+
+def trainLogisticRegressionModel(data):
+  tokenizer = Tokenizer().setInputCol("comment_text").setOutputCol("words")
+  remover= StopWordsRemover().setInputCol("words").setOutputCol("filtered").setCaseSensitive(False)
+  hashingTF = HashingTF().setNumFeatures(1000).setInputCol("filtered").setOutputCol("rawFeatures")
+  idf = IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+  lr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10)
+  pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, lr])
+
+  paramGrid = ParamGridBuilder()\
+    .addGrid(hashingTF.numFeatures,[1000])\
+    .addGrid(lr.regParam, [0.1])\
+    .addGrid(lr.elasticNetParam, [0.3])\
+    .build()
+
+  crossval = TrainValidationSplit(estimator=pipeline,
+                            estimatorParamMaps=paramGrid,
+                            evaluator=BinaryClassificationEvaluator().setMetricName('areaUnderPR'), # set area Under precision-recall curve as the evaluation metric
+                            # 80% of the data will be used for training, 20% for validation.
+                            trainRatio=0.8)
+
+  cvModel = crossval.fit(data)
+  modelName = "LogisticRegressionModel"
+  cvModel.bestModel.write().overwrite().save(modelName)
+
+  return modelName
+
+
+def trainNaiveBayesModel(data):
+  tokenizer = Tokenizer().setInputCol("comment_text").setOutputCol("words")
+  remover= StopWordsRemover().setInputCol("words").setOutputCol("filtered").setCaseSensitive(False)
+  hashingTF = HashingTF().setNumFeatures(1000).setInputCol("filtered").setOutputCol("rawFeatures")
+  idf = IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+  nb = NaiveBayes(labelCol="label", featuresCol="features")
+  pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, nb])
+
+  paramGrid = ParamGridBuilder()\
+    .addGrid(hashingTF.numFeatures,[1000]) \
+    .addGrid(nb.smoothing, [1]) \
+    .build()
+
+  crossval = TrainValidationSplit(estimator=pipeline,
+                            estimatorParamMaps=paramGrid,
+                            evaluator=BinaryClassificationEvaluator().setMetricName('areaUnderPR'), # set area Under precision-recall curve as the evaluation metric
+                            # 80% of the data will be used for training, 20% for validation.
+                            trainRatio=0.8)
+
+  cvModel = crossval.fit(data)
+  modelName = "NaiveBayesModel"
+  cvModel.bestModel.write().overwrite().save(modelName)
+
+  return modelName
+
+
+def trainGradientBoostModel(data): 
+  tokenizer = Tokenizer().setInputCol("comment_text").setOutputCol("words")
+  remover= StopWordsRemover().setInputCol("words").setOutputCol("filtered").setCaseSensitive(False)
+  hashingTF = HashingTF().setNumFeatures(1000).setInputCol("filtered").setOutputCol("rawFeatures")
+  idf = IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+  gbt = GBTClassifier(labelCol="label", featuresCol="features", maxIter=10)
+  pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, gbt])
+
+  cvModel = pipeline.fit(data)
+  modelName = "BoostTreeModel"
+  cvModel.write().overwrite().save(modelName)
+  return modelName
+
+
+def trainBinaryTreeModel(data):
+  tokenizer = Tokenizer().setInputCol("comment_text").setOutputCol("words")
+  remover= StopWordsRemover().setInputCol("words").setOutputCol("filtered").setCaseSensitive(False)
+  hashingTF = HashingTF().setNumFeatures(1000).setInputCol("filtered").setOutputCol("rawFeatures")
+  idf = IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+  dt = DecisionTreeClassifier(labelCol="label", maxDepth=30,featuresCol="features")
+  pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, dt])
+
+  paramGrid = ParamGridBuilder()\
+            .addGrid(dt.maxDepth, [2]) \
+            .addGrid(dt.maxBins, [10]) \
+            .build()
+
+  crossval = TrainValidationSplit(estimator=pipeline,
+                            estimatorParamMaps=paramGrid,
+                            evaluator=BinaryClassificationEvaluator().setMetricName('areaUnderPR'), # set area Under precision-recall curve as the evaluation metric
+                            # 80% of the data will be used for training, 20% for validation.
+                            trainRatio=0.8)
+
+  cvModel = crossval.fit(data)
+  modelName = "BinaryTreeModel"
+  cvModel.bestModel.write().overwrite().save(modelName)
+
+  return modelName
+
+
+
---
+++ driver.py
@@ -0,0 +1,130 @@
+import os
+import re
+import pyspark
+import pandas as pd
+import numpy as np
+
+from pyspark.context import SparkContext
+
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import ArrayType, StringType, IntegerType, StructType, StructField
+from pyspark.sql.functions import *
+
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit, CrossValidator
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+
+from pyspark.ml.linalg import Vector
+from pyspark.ml import Pipeline, PipelineModel
+
+from pyspark.ml.feature import RegexTokenizer, HashingTF, StopWordsRemover, IDF, Tokenizer
+
+from nltk.stem.snowball import SnowballStemmer
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+from classificationModels import trainLinearSVCModel, trainLogisticRegressionModel, trainNaiveBayesModel, trainGradientBoostModel, trainBinaryTreeModel
+
+print(pyspark.__version__)
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64/jre"
+os.environ["SPARK_HOME"] = "venv/lib/python3.6/site-packages/pyspark"
+
+TRAIN_FILE = "data/preprocessed/preprocessedDataWikiTrain"
+TEST_FILE = "data/preprocessed/preprocessedDataWikiTest"
+OTHER_DATA_FILE = "data/preprocessed/preprocessedDataLDT"
+
+customSchema = StructType([
+  StructField("id", StringType(), True),
+  StructField("comment_text", StringType(), True),
+  StructField("toxic", IntegerType(), True),
+  StructField("severe_toxic", IntegerType(), True),
+  StructField("obscene", IntegerType(), True),
+  StructField("threat", IntegerType(), True),
+  StructField("insult", IntegerType(), True),
+  StructField("identity_hate", IntegerType(), True),
+  StructField("clean", IntegerType(), True)]
+)
+
+customSchema2 = StructType([
+  StructField("id", IntegerType(), True),
+  StructField("count", IntegerType(), True),
+  StructField("hate_speech", IntegerType(), True),
+  StructField("offensive_language", IntegerType(), True),
+  StructField("neither", IntegerType(), True),
+  StructField("class", IntegerType(), True),
+  StructField("comment_text", StringType(), True),
+  StructField("label", IntegerType(), True)]
+)
+
+def nullCheck(df):
+  print("Check null values")
+  df.where(df.comment_text.isNull()).show()
+  df.where(df.comment_text == '').show()
+
+
+def binaryData(spark):
+  training_spark_df_binary = spark.read.csv(TRAIN_FILE, header='true', schema=customSchema)
+
+  training_spark_df_binary = training_spark_df_binary.withColumnRenamed("clean", "label")
+  training_spark_df_binary = training_spark_df_binary.drop('toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate')
+
+  testing_spark_df_binary = spark.read.csv(TEST_FILE, header='true', multiLine=True, escape="\"", schema=customSchema)
+
+  testing_spark_df_binary = testing_spark_df_binary.withColumnRenamed("clean", "label")
+  testing_spark_df_binary = testing_spark_df_binary.drop('toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate')
+
+  ldt = spark.read.csv(OTHER_DATA_FILE, header='true', schema=customSchema2)
+  ldt = ldt.drop("count", "hate_speech", "offensive_language", "neither", "class")
+
+  return training_spark_df_binary, testing_spark_df_binary, ldt
+
+
+def evaluateModel(model, data1, data2, data3):
+  persistedModel = PipelineModel.load(model)
+
+  train_prediction = persistedModel.transform(data1)
+  test_prediction = persistedModel.transform(data2)
+  otherDatasetTest = persistedModel.transform(data3)
+
+  pd_prediction = test_prediction.select("*").toPandas()
+  actual = pd_prediction["label"].tolist()
+  pred = pd_prediction["prediction"].tolist()
+
+  pd_prediction_other_dataset = otherDatasetTest.select("*").toPandas()
+  actual_otherdataset = pd_prediction_other_dataset["label"].tolist()
+  pred_otherdataset = pd_prediction_other_dataset["prediction"].tolist()
+
+  tn, fp, fn, tp = confusion_matrix(actual, pred).ravel()
+  print(confusion_matrix(actual, pred))
+  print("true positive rate",tp / (tp + fp))
+  print("true negative rate",tn / (tn + fp))
+
+  # compute the accuracy score on training data
+  correct_train = train_prediction.filter(train_prediction.label == train_prediction.prediction).count()  
+  accuracy_train = correct_train/train_prediction.count() # store the training accuracy score
+  print('Training set accuracy {:.2%} data items: {}, correct: {}'.format(accuracy_train,train_prediction.count(), correct_train))
+      
+  # Caculate the accuracy score for the best model 
+  correct_test = test_prediction.filter(test_prediction.label == test_prediction.prediction).count()  
+  accuracy_test = correct_test/test_prediction.count()
+  print('Testing set accuracy {:.2%} data items: {}, correct: {}'.format(accuracy_test, test_prediction.count(), correct_test))
+      
+  # Caculate the accuracy score for the best model 
+  correct_test_otherDataset = otherDatasetTest.filter(otherDatasetTest.label == otherDatasetTest.prediction).count()  
+  accuracy_test_otherDataset = correct_test_otherDataset/otherDatasetTest.count()
+  print('Testing set accuracy for other dataset is  {:.2%} data items: {}, correct: {}'.format(accuracy_test_otherDataset, otherDatasetTest.count(), correct_test_otherDataset))
+
+
+if __name__ == '__main__':
+  sc = SparkContext('local')
+  spark = SparkSession(sc)
+
+  training_spark_df_binary, testing_spark_df_binary, ldt = binaryData(spark)
+  modelName = trainLogisticRegressionModel(training_spark_df_binary)
+
+  evaluateModel(modelName, training_spark_df_binary, testing_spark_df_binary, ldt)
+
+
