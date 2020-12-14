9aaa5cac2263f0a473fab3505d87178920ad5482
William Zou <wyzou@uwaterloo.ca>
2020-12-14 04:11:27 -0500

    Add more toy examples

--- playground/example.py
+++ playground/example.py
@@ -4,3 +4,19 @@
   idf = IDF() ...
   lr = LinearSVC(labelCol="label", featuresCol="features", ...)
   pipeline=Pipeline(stages=[tokenizer, remover, hashingTF, idf, lr])
+
+
+
+
+stemmer = SnowballStemmer("english")
+def stemming(sentence):
+    stemSentence = ""
+    for word in sentence.split():
+        stem = stemmer.stem(word)
+        stemSentence += stem
+        stemSentence += " "
+    stemSentence = stemSentence.strip()
+    return stemSentence
+
+stemmer_udf = udf(lambda line: stemming(line), StringType())
+train = train.withColumn("comment_text", stemmer_udf("comment_text"))
